10fe91dc6bd31c062821293aea9db5f10b766122
Brownei <esitibrownson@gmail.com>
2026-05-15 19:08:17 +0100

    Chore: The new updated config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,20 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+     typescript = { "prettier" },
+    javascript = { "prettier" },
+     rust = { "rust-analyzer" },
+    go = { "goimports", "gopls" },
+    cs = { "csharpier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,19 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = {
+  "html",
+  "cssls",
+  "ts_ls",
+  "bashls",
+  "lua_ls",
+  "rust_analyzer",
+  "gopls",
+  "cairo_ls",
+  "markdown_oxide",
+  "tailwindcss",
+  -- "roslyn",
+  "omnisharp"
+}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
